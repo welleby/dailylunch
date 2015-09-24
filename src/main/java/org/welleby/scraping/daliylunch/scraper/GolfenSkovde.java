f1bc32f4cb0e52092070e28815570e5ba09eba2a
Tommy Welleby <tommywelleby@gmail.com>
2015-09-24 22:08:19 +0200

    Updated scrapers to use jsoup

--- src/main/java/org/welleby/scraping/daliylunch/scraper/GolfenSkovde.java
+++ src/main/java/org/welleby/scraping/daliylunch/scraper/GolfenSkovde.java
@@ -1,27 +1,32 @@
 package org.welleby.scraping.daliylunch.scraper;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.time.DayOfWeek;
 
-import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class GolfenSkovde extends Scraper {
+	private final String divName;
 
-	public GolfenSkovde() throws IOException {
+	public GolfenSkovde() throws Exception {
 		super();
 		this.mainUrl = new URL("http://mattiasmat.se/golfen/");
-		this.connection = mainUrl.openConnection();
+		this.divName = "lunch-content-golfen";
+
 	}
 	@Override
 	public String getLunch(DayOfWeek day) throws IOException {
 
-		InputStream in = connection.getInputStream();
-		String encoding = connection.getContentEncoding();
-		encoding = encoding == null ? "UTF-8" : encoding;
-		String body = IOUtils.toString(in, encoding);
-		System.out.println(body);
+		Document doc = Jsoup.connect(mainUrl.toString()).get();
+		Element lunchDiv = doc.getElementById(divName);
+		Elements lunchElements = lunchDiv.getAllElements();
+		for (Element element : lunchElements) {
+			System.out.println(element.toString());
+		}
 		
 		return null;
 	}
--- src/main/java/org/welleby/scraping/daliylunch/scraper/Scraper.java
+++ src/main/java/org/welleby/scraping/daliylunch/scraper/Scraper.java
@@ -1,12 +1,10 @@
 package org.welleby.scraping.daliylunch.scraper;
 
 import java.net.URL;
-import java.net.URLConnection;
 import java.time.DayOfWeek;
 
 public abstract class Scraper {
 	protected URL mainUrl;
-	protected URLConnection connection;
 	
 	public abstract String getLunch(DayOfWeek day) throws Exception;
 	public URL getMainUrl() {
