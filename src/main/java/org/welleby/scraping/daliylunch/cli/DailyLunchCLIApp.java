fc2ddbbca3bc894afe7ecf0027968ed62c2c28f8
Tommy Welleby <tommywelleby@gmail.com>
2015-10-03 21:22:57 +0200

    CLIApp now returns todays scraperresults from all scrapers

--- src/main/java/org/welleby/scraping/daliylunch/cli/DailyLunchCLIApp.java
+++ src/main/java/org/welleby/scraping/daliylunch/cli/DailyLunchCLIApp.java
@@ -6,21 +6,31 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.welleby.commons.time.DayOfWeekHelper;
 import org.welleby.scraping.daliylunch.scraper.GladjeSkovde;
+import org.welleby.scraping.daliylunch.scraper.GolfenSkovde;
 import org.welleby.scraping.daliylunch.scraper.LunchScraper;
+import org.welleby.scraping.daliylunch.scraper.MammaMiaSkovde;
+import org.welleby.scraping.daliylunch.scraper.PinchosSkovde;
 
 public class DailyLunchCLIApp {
 
 	private List<String> lunch = new ArrayList<String>();
 	
 	public static void main(String[] args) throws Exception {
-		LunchScraper scraper = new GladjeSkovde();
+		List<LunchScraper> scrapers = new ArrayList<LunchScraper>();
+		scrapers.add(new GladjeSkovde());
+		scrapers.add(new GolfenSkovde());
+		scrapers.add(new MammaMiaSkovde());
+		scrapers.add(new PinchosSkovde());
 		
 		Calendar now = new GregorianCalendar(); 
-		DayOfWeek today = DayOfWeek.of(now.get(Calendar.DAY_OF_WEEK));
-		System.out.println(today.toString() + " : \r\n");
-		System.out.println(scraper.getRestaurantName() +" : "+ scraper.getLunch(today));
+		DayOfWeek today = DayOfWeekHelper.getDay(now);
 		
+		System.out.println(today.toString() + " : \r\n");
+		for (LunchScraper scraper : scrapers) {
+			System.out.println(scraper.getRestaurantName() +" : "+ scraper.getLunch(today));
+		}
 		
 
 	}
